3930bc957af2e54f6c5d58f9f6ebbab746285c6a
Ruza <vinny3@seznam.cz>
2019-10-21 09:22:28 +0200

    LDR+LED1

--- SENZOR/senzor.ino
+++ SENZOR/senzor.ino
@@ -1,26 +1,46 @@
-#define PIR 2       // definice promenne PIR = 2
-#define LED 3       // definice promenne LED = 3
 
-int pirState;       // promenna - stav Cidla PIR
+// Pri zaznamenani pohybu za tmy pohybovym cidlem, bude rozsvicena LED = hlavni svetlo, pokud je tma a nebude sepnute cidlo, tak bude svítit LED1
+// Pohybove cidlo a LED svìtla budou funkcni pouze za tmy.
+// --------------------------------------------------------
+#define LDR 4      // LDR rezistor
+#define PIR 2      // pohybove cidlo 
+#define LED 3      // hlavni led svetlo 
+#define LED1 9     // oznacujici vchodove dvere PWM
 
-void setup() {                   // nastavi pocatecni hodnoty programu
+int pirState;       // stav Cidla PIR
+int ldrValue;       // stav LDR rezistoru
+
+void setup() {                   
   Serial.begin(9600);            // zapne serial komunikaci s PC po USB
-  pinMode(LED, OUTPUT);          // nastavi pin 3 jako vystup - pro LED = svetlo
+  pinMode(LED, OUTPUT);          
+  pinMode(LED1, OUTPUT);         
   pinMode(PIR, INPUT);           // nastavi pin 2 jako vstup - vystup z PIR cidla
+  pinMode(LDR, INPUT);           // nastavi pin 4 jako vystup z LDR
+  digitalWrite(LED, LOW);        // nastavi LED = svetlo, aby nesvitilo po spusteni programu
+  analogWrite(LED1, 0);          // nastavi LED1 = aby nesvitila po spusteni programu
 }
 
-
-
 void loop(){
+  ldrValue = analogRead(LDR);              // cte hodnotu ze vstupu LDR
+  Serial.print(" Analog vstup LDR = ");    // zobrazeni hodnoty ze vstupu LDR
+  Serial.println(ldrValue);                // terminal na PC
 
-  if (pirState == HIGH) {                // pokud je PIR aktivovano
-      digitalWrite(LED,HIGH);            // rozsvit LED = svetlo
-      delay(1000);                       // cekej 1 vterin
-      digitalWrite(LED,LOW);             // zhasni LED = svetlo
-      delay(1000);                       // cekej 1 vterinu
+  if (ldrValue >= 646) {                   // pokud je tma
+    analogWrite(LED1, 2);                  // rozsvit LED1 = LED1 sviti vždy za tmy a intenzitou 2
+    pirState = digitalRead(PIR);           
+    Serial.print(" PIR je ve stavu = ");   
+    Serial.println(pirState);              
+
+    if (pirState == HIGH) {                // pokud je PIR aktivovano
+      analogWrite(LED1, 0);                // zhasni LED1
+      digitalWrite(LED, HIGH);             // rozsvit LED = svetlo
+      delay(15000);                        // cekej 15 vterin - sviti LED
+      digitalWrite(LED, LOW);              // zhasni LED = svetlo
+    } 
+    else {                                 // pokud PIR neaktivni
+      digitalWrite(LED, LOW);              // LED = svetlo je vypnuto
+    }
   }
-  else {                                 // pokud PIR neaktivni
-      digitalWrite(LED, LOW);            // LED = svetlo je vzpnuto
-  }
-      delay(1000);                             // cekej 1 vterinu
-}                                              
+  else
+    analogWrite(LED1, 0);                  // zhasni LED1, kdyz neni tma
+}
